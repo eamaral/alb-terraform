3cc7766ca9af1d1e7789dbd13d200b12aff40208
Erik <erik.fernandes87@gmail.com>
2025-03-29 18:55:04 -0300

    workflow dispatch

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "region" {
   description = "Região da AWS"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "ID da VPC onde o ALB será criado"
   type        = string
+  default     = ""
 }
 
 variable "private_subnets" {
   description = "Subnets privadas associadas ao ALB"
   type        = list(string)
+  default     = []
 }
