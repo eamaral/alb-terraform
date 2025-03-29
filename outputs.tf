08973ec3961b33ad57e190d5b8358177a4d771d4
Erik <erik.fernandes87@gmail.com>
2025-03-29 18:58:31 -0300

    workflow dispatch

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
 output "alb_dns_name" {
-  description = "DNS do ALB"
+  description = "DNS do Load Balancer"
   value       = aws_lb.fastfood_alb.dns_name
 }
 
 output "alb_target_group_arn" {
-  description = "ARN do Target Group do ALB"
+  description = "ARN do Target Group"
   value       = aws_lb_target_group.backend_tg.arn
 }
 
 output "alb_sg_id" {
-  description = "ID do Security Group do ALB"
+  description = "Security Group ID do ALB"
   value       = aws_security_group.alb_sg.id
+}
+
+output "alb_listener_arn" {
+  description = "ARN do Listener do ALB"
+  value       = aws_lb_listener.http.arn
 }
